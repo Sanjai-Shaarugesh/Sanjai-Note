db50271c936572a07f2cc60ca177044abc20ccea
Sanjai <167017833+Sanjai-Shaarugesh@users.noreply.github.com>
2024-05-01 18:48:06 +0530

    small update

--- Webapp/webapp/src/components/Home.jsx
+++ Webapp/webapp/src/components/Home.jsx
@@ -9,10 +9,6 @@ function Home() {
   const [message, setMessage] = useState("");
   const [title, setTitle] = useState("");
   const [note_image, setImage] = useState(null);
- 
-
-  
-
 
   {
     /*const [badge, setBadge] = useState("");*/
@@ -28,7 +24,6 @@ function Home() {
       .then((res) => res.data)
       .then((data) => {
         setNotes(data);
-       
       })
       .catch((err) => alert(err));
   };
@@ -49,8 +44,9 @@ function Home() {
 
     const formData = new FormData();
     formData.append("title", title);
+
     formData.append("message", message);
-  
+
     if (note_image) {
       formData.append("note_image", note_image);
     }
@@ -76,54 +72,120 @@ function Home() {
       </video>
 
       <div>
-
         <div className="bag">
           <div>
-            <h2 className="label">Notes</h2>
+            <h2 className="label ">Notes</h2>
             {notes.map((note) => (
               <Note note={note} onDelete={deleteNote} key={note.id} />
             ))}
           </div>
-          <h2 className="label">Create a Note</h2>
-          <form className="bag" onSubmit={createNote}>
-            <label htmlFor="title" className="label">
-              Title:
-            </label>
-            <br />
-            <input
-              type="text"
-              id="title"
-              name="title"
-              required
-              onChange={(e) => setTitle(e.target.value)}
-              value={title}
-            />
+          <h2 className="font-sans text-white font-bold  text-center text-5xl my-20">Create a Note</h2>
+          <form
+            className=" align-content: center max-w-sm:50rem mx-auto "
+            onSubmit={createNote}
+          >
+            <div className="max-w-sm:50rem  mx-auto">
+              <label className="font-mono text-white font-bold">
+                
+                
+                Title :
+              </label>
+              <br />
+              <input
+                type="text"
+                id="title"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Title"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </div>
 
-            <label htmlFor="message" className="label">
-              Message:
-            </label>
+            <div className="mb-5">
+              <label htmlFor="content " className="font-mono text-white font-bold">Content</label>
+              <br />
+              <textarea
+                type="text"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  font-mono ..."
+                placeholder="content"
+                required
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+            </div>
+
             <br />
-            <textarea
-              id="content"
-              name="content"
-              
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-            <br />
-            
-            <label htmlFor="image" className="label">
+
+            {/* <label htmlFor="image" className="label">
               Image:
             </label>
             <br />
             <input
               id="image"
               name="image"
-              
               type="file"
               onChange={(e) => setImage(e.target.files[0])}
-            />
-            <input type="submit" value="Submit"></input>
+          />*/}
+
+            <div className="flex items-center justify-center w-full">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    SVG, PNG, JPG or GIF (MAX. 800x400px)
+                  </p>
+                </div>
+                <input
+                  id="dropzone-file"
+                  type="file"
+                  className="hidden"
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+              </label>
+            </div>
+
+            {/*<div className=" container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+ <button type="submit" className="   text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+ </div>
+        */}
+
+            <div className=" container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+            <a  className="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+                <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+                <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+                  <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+                  <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+                </span>
+                <button type="submit" className="relative text-white">
+                  Submit
+                </button>
+              </a>
+            </div>
+
+            {/*<input type="submit" value="Submit"></input>*/}
           </form>
 
           <a href="/logout" className="link">
@@ -132,8 +194,6 @@ function Home() {
 
           <a href="https://sanjai-shaarugesh.netlify.app" className="link2">
             <h1 className="link2">This webapp is created by shaarugesh</h1>
-            
-             
           </a>
         </div>
       </div>
