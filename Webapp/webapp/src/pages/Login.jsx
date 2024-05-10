b5c71e67e42576ce26cdb9cdbf4a57a96296df4a
Sanjai <167017833+Sanjai-Shaarugesh@users.noreply.github.com>
2024-05-10 20:17:52 +0530

    few changes

--- Webapp/webapp/src/pages/Login.jsx
+++ Webapp/webapp/src/pages/Login.jsx
@@ -1,11 +1,21 @@
-import Form from "../components/Form"
-
+import Form from "../components/Form";
+import img from "../assets/s.png";
 
 function Login() {
-    return (<div><Form route="/api/token/" method="login" />
-     <a href="/register" className="link"> <h1 className="link">Doesn't have a account click here</h1></a>
-  
-    </div>)
+  return (
+    <div >
+      <Form route="/api/token/" method="login" /> <div className="flex justify-center items-center h-screen:10rem">
+      <a href="/register" className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+        
+        <img aria-hidden="true" className="w-5 h-5 me-3" src={img} alt="Icon" />
+        <span className="w-full">Don't have an account? Click here.</span>
+      
+        <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg>
+      </a></div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
