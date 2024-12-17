46ea2f0e04e8a4382f9818587c7f967f8a4794d6
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2024-12-18 01:54:01 +0530

    few ui and bug fixes

--- Webapp/webapp/src/components/Home.jsx
+++ Webapp/webapp/src/components/Home.jsx
@@ -294,7 +294,8 @@ function Home() {
             </div>
 
             <div className=" container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
-              <a  className="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+
+              <a className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
                 <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
                 <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
                   <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
@@ -318,6 +319,6 @@ function Home() {
       </div>
     </div>
   );
-}
 
+}
 export default Home;
