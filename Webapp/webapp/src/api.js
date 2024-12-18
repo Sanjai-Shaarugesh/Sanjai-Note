717752b011e5e6ed636413112a6e7d89b2cafb6b
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2024-12-18 13:33:03 +0530

    few changes

--- Webapp/webapp/src/api.js
+++ Webapp/webapp/src/api.js
@@ -5,7 +5,7 @@ import { ACCESS_TOKEN } from "./constants";
 
 
 const api = axios.create({
-  baseURL:  process.env.VITE_API_URL,
+  baseURL:  import.meta.env.VITE_BASE_URL,
 });
 
 api.interceptors.request.use(
