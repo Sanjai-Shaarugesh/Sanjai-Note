0ca457ad3a052c27a54ada818ea64fbdb4dbc1a1
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2024-12-18 13:57:47 +0530

    few changes

--- Webapp/webapp/src/api.js
+++ Webapp/webapp/src/api.js
@@ -5,7 +5,7 @@ import { ACCESS_TOKEN } from "./constants";
 
 
 const api = axios.create({
-  baseURL:"https://shaarugesh.pythonanywhere.com",
+  baseURL:  import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
